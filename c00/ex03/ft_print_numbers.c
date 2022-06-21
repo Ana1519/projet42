1e0745c316cd3d26f166438b35106670cb32efce
Ana1519 <cantaisanais@gmail.com>
2022-06-21 19:16:33 +0200

    Ajout ft_print_numbers.c

---
+++ c00/ex03/ft_print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void ft_print_numbers(void);
+
+	 ft_print_numbers();
+
+void ft_print_numbers(void)
+{
+	write( 1, "0123456789", 9);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
